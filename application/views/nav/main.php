d6f88332616c8d767cac535af90144256cdcf668
Apm Server <apm@server>
2014-03-10 18:37:09 +0700

    daily update

--- application/views/nav/main.php
+++ application/views/nav/main.php
@@ -1,10 +1,19 @@
 <div class="app-nav-wrapper">
     <a class="brand">brand</a>
     <ul class="dropdown-menu-square">
-        <li class="active"><a href="#"><i class="fa fa-home fa-fw"></i> Home</a></li>
-        <li><a href="#">Lorem</a></li>
+        <li class="active">
+            <a href="#"><i class="fa fa-home fa-fw"></i><span>Home</span></a>
+        </li>
+        <li class="active">
+            <a href="#"><i class="fa fa-user fa-fw"></i><span>User</span></a>
+        </li>
+        <li class="active">
+            <a href="#"><i class="fa fa-tasks fa-fw"></i><span>Task</span></a>
+        </li>
         <li class="dropdown-submenu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown</a>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <i class="fa fa-th-list fa-fw"></i><span>Menu</span>
+            </a>
             <ul class="dropdown-menu">
                 <li><a href="#">Action</a></li>
                 <li><a href="#">Another action</a></li>
